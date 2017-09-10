2f5fe9aa8edb45bf6785734fc4ff834d360593ac
Warren Buckley <wbuckley@Warrens-MBP.home>
2017-09-10 18:23:49 -0400

    Fixed start script

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('*', (req, res) => {
 });
 
 // Get port from envrionment and store in Express.
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '8080';
 app.set('port', port);
 
 // Create HTTP server.
